352b201f3f15473c171a1c23b483c1e630cfe7d3
UtileHomme <jatinsharma@healthheal.in>
2018-02-25 09:20:13 +0530

    Added Important terms for Economics

---
+++ Important_terms.php
@@ -0,0 +1,13 @@
+<!-- What is MicroEconomics -->
+- it is the study of individual actors
+- these actors could be firms, households
+- how actors make decisions / allocation of scarce resources (Eg - Food, Water, Labour)
+- how does it affect prices and everything else
+- how people think
+- Finally we make some assumptions or simplify the above question-answers
+
+<!-- What is MacroEconomics  -->
+- the study of economy in aggregrate
+- aggregate economy policy decisions
+Eg - What is going to happen when you raise or lower taxes
+- How does it affect the overall productivity
