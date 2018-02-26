6b30acc3dbe91c5928bc439eb6f7028e556ca768
UtileHomme <jatinsharma@healthheal.in>
2018-02-26 19:10:34 +0530

    Taxes concepts added

---
+++ All_About_Taxes.php
@@ -0,0 +1,54 @@
+<!-- What is the Tax Year -->
+- Previous year or financial year or your tax year is the 12 months period that begins on 1st APRIL and
+ends on the 31st March of the next year
+- No matter when we start our job, the tax year closes on 31st March and a new Tax year starts on 1st April
+
+<!-- What is the Assessment Year -->
+- It is the financial year after the TAX YEAR
+- It is the year in which one files the tax return for the previous year
+
+Eg -
+If one starts his job on 1st January 2015, the tax year closes on 31st March 2015
+- 2014-15 is the TAX YEAR and the ASSESSMENT YEAR is 2015-16
+- Last date is 31st July 2015
+
+<!-- What is the Income on which one pays TAX -->
+
+a. Income from salary
+- Salary, allowances, leave encashment basically all the money one receives while rendering your job
+as a result of the employment agreement
+
+b. Income from House Property
+- Income from house or building, this may be owned and self occupied or may be rented
+
+c. Income from Gain
+- Income from gain or loss when you sell a capital asset
+
+d. Income from Business or Profession
+- Income/loss that arises as a result of carrying on a business or profession
+
+e. Income from Other Sources
+- This is the residual head
+- it includes your income from savings bank account, fixed deposits, family pensions or gifts received
+
+<!-- What are Deductions -->
+- Deductions reduce the Gross Income
+- These are the amounts Income Tax allows one to reduce from your Income, thus pay lower tax
+
+Gross Income - Sum of all heads of Income
+Taxable Income - Gross Income Less Deductions
+
+** The more you make use of the deductions allowed, the lower your tax will be
+- these are allowed under section 80 of the Income Tax Act (Section 80C to 80U)
+
+<!-- Make the Section 80C  your best friend -->
+- Section 80C can take off Rs 150000 from your Gross Income
+- When we open a PPF account, we need to deposit minimum of Rs 500 and we can deposit a max of Rs. 150000
+in a year
+- Money deposited in a PPF account grows each year
+- we can claim deductions from it
+
+<!-- What is TDS -->
+- Tax Deduction at Source
+- The person's employer will estimate the total annual income and deduct tax on the income ,if the taxable income exceeds Rs 2,50000
+- When we earn Interest on Fixed Deposit, the bank deducts the TDS
--- Important_terms.php
+++ Important_terms.php
@@ -11,3 +11,11 @@
 - aggregate economy policy decisions
 Eg - What is going to happen when you raise or lower taxes
 - How does it affect the overall productivity
+
+<!-- What is Scarcity -->
+- Economics is the study of "how we allocate the scarce resources"
+- If there's more demand for a scarce resource, determining who gets it, how much of it do they get
+and what do they have to give up to get those scarce resources
+
+Eg - Caviar (fish egss) is a scarce resource
+- Even "labour" is a resource (it is a scarce resource)
