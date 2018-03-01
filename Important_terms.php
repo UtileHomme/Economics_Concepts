baaf0ce040f60fc262d04b334b7f3da82489f57c
UtileHomme <jatinsharma@healthheal.in>
2018-03-01 16:48:32 +0530

    Added Normative and Positive Statements Explanations

--- Important_terms.php
+++ Important_terms.php
@@ -19,3 +19,17 @@ and what do they have to give up to get those scarce resources
 
 Eg - Caviar (fish egss) is a scarce resource
 - Even "labour" is a resource (it is a scarce resource)
+
+<!-- Difference between Normative and Positive Statements -->
+
+1. Normative Statement
+- it is a statement concerned with giving an opinion or involving ethics
+- it how someone things the world should be
+
+Eg - Paying People who aren't working, even though they could work, is wrong and unfair
+Eg - The govt. should raise taxes on the wealthy to pay for helping the poor 
+
+2. Positive Statement
+- it doesn't necessarily have to be true but can be tested
+Eg - Programs like welfare reduce the incentive for people to work
+Eg - Raising taxes on the wealthy to pay for govt. programs grows the economy
